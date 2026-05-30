dfbb066f8afdd0da7c4844237cd752e26b793bb7
Krithika K Bhat <krithikabhat2006@gmail.com>
2026-05-31 01:39:33 +0530

    Modify Flask app to use environment variables for host and port
    Update app to run on specified host and port from environment variables.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask_cors import CORS
 from flask import Flask, request, jsonify
 from deepface import DeepFace
@@ -31,4 +32,7 @@ def analyze():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
